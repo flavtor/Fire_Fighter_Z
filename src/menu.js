fe5ca18eccf03297ec3f08af369471cc493dd260
Maxime Petit <contactmaximepetit@gmail.com>
2023-01-29 23:01:35 +0100

    [ADD] game menu

---
+++ src/menu.js
@@ -0,0 +1,30 @@
+import initGame from "./game";
+
+export default class GameMenu {
+  constructor(options) {
+    this.options = options;
+    this.play = false;
+    this.createMenu();
+    this.gameLaunch();
+  }
+
+  createMenu() {
+    this.optionsList = document.querySelector(".options");
+    for (let i = 0; i < this.options.length; i++) {
+      const option = document.createElement("button");
+      option.classList.add(this.options[i].toLowerCase());
+      option.innerText = this.options[i];
+
+      this.optionsList.appendChild(option);
+    }
+  }
+
+  gameLaunch() {
+    const playButton = document.querySelector(".play");
+    const menu = document.querySelector(".menu");
+    playButton.addEventListener("click", () => {
+      menu.classList.add("disappear");
+      initGame();
+    });
+  }
+}
