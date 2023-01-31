d11a84d641a03da7836e2e283700dd691d683085
agahakan <qirahakan@gmail.com>
2023-01-31 20:49:15 +0100

    update: changed method to POST and added registration success msg

--- src/login-page.js
+++ src/login-page.js
@@ -5,6 +5,7 @@ const loginErrorMsg = document.getElementById("login-error-msg");
 const registerForm = document.getElementById("register-form");
 const registerButton = document.getElementById("register-form-submit");
 const registerErrorMsg = document.getElementById("register-error-msg");
+const registerSuccessMsg = document.getElementById("register-success-msg");
 
 // Service REST /login parameter username, password 
 async function login(){
@@ -38,15 +39,17 @@ async function register(){
 	const password = registerForm.password.value;
 
 	let response = await fetch('http://localhost:8080/adduser?username='+username+'&password='+password, {
-		method: 'GET',
+		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		}
 	});
 	let data = await response.json()
 	
-	if (data["status"] == "ok"){
+	if (data["status"] == "ok") {
 		localStorage.setItem("username", data["username"]);
+		registerSuccessMsg.style.opacity = 1;
+
 	}
 	else {
 		registerErrorMsg.style.opacity = 1;
