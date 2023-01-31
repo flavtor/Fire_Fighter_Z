b3872119845dcfe37418b5fff7c99483f08030e6
flavtor <72008799+flavtor@users.noreply.github.com>
2023-01-31 02:10:37 +0100

    [ADD] mana gestion

--- dist/index.c3fdd8eb.js
+++ dist/index.c3fdd8eb.js
@@ -680,6 +680,7 @@ class Card {
     constructor(){
         this.cards = [];
         this.json_obj = [];
+        this.loading = false;
     }
     initClickEvent(data) {
         this.cards.forEach((card)=>{
@@ -704,6 +705,7 @@ class Card {
                 "Content-Type": "application/json"
             }
         });
+        this.showLoading();
         let data = await response.json();
         localStorage.setItem("listCards", JSON.stringify(data));
         const listCards = localStorage.getItem("listCards");
@@ -745,6 +747,11 @@ class Card {
         this.json_obj = JSON.parse(listCards);
         this.createCards(this.json_obj);
     }
+    showLoading() {
+        this.loading = true;
+        const loadingElement = document.querySelector(".loading");
+        loadingElement.classList.add("disappear");
+    }
 }
 exports.default = Card;
 
@@ -755,14 +762,33 @@ var _allgo = require("./allgo");
 var _allgoDefault = parcelHelpers.interopDefault(_allgo);
 var hp_p = 180;
 var hp_m = 100;
+var mana = 3;
 let turn = 1;
 let iturn = 1;
-let defense = 0;
+let defence_p = 0;
+let turndef_p = 0;
+let defence_m = 0;
+let turndef_m = 0;
 function playerturn(activeCard) {
     hp_monster = document.getElementById("monster_hp");
     hp_player = document.getElementById("player_hp");
-    let nbr = (0, _allgoDefault.default)(activeCard.Attack, activeCard.Defence, activeCard.Heal, false, false);
-    console.log("test");
+    mana_points = document.getElementById("mana_p");
+    console.log(activeCard);
+    mana -= activeCard.Cost;
+    mana_points.textContent = mana;
+    if (mana <= 0) {
+        alert("no more mana, you must end turn");
+        turndef_m -= 1;
+        if (turndef_m <= 0) defence_m = 0;
+        turn = 2;
+        iturn += 1;
+        mana = 3;
+        mana_points.textContent = mana;
+        return;
+    }
+    let nbr = (0, _allgoDefault.default)(activeCard.Attack, defence_m, activeCard.Heal, false, false);
+    defence_p += activeCard.Defence;
+    if (defence_p > 0 && turndef_p == 0) turndef_p = 2;
     if (nbr >= 100) {
         hp_p += nbr - 100;
         hp_player.textContent = hp_p;
@@ -775,12 +801,48 @@ function playerturn(activeCard) {
     console.log("hp :", hp_m);
     hp_monster.textContent = hp_m;
     console.log("test", nbr);
-    //hp_monster.txt(hp_m);
     if (hp_m <= 0) alert("You win!");
+    turndef_m -= 1;
+    if (turndef_m <= 0) defence_m = 0;
     turn = 2;
     iturn += 1;
 }
-exports.default = playerturn;
+function monsterattack(hp_player1) {}
+function monsterheal(hp_monster1) {
+    return;
+}
+function monsterdefence() {
+    return;
+}
+function monsterturn(nbr) {
+    hp_player = document.getElementById("player_hp");
+    hp_monster = document.getElementById("monster_hp");
+    alea = Math.floor(Math.random() * 10 + 1);
+    switch(nbr){
+        //attack
+        case 1:
+            monsterattack(hp_player, alea);
+            break;
+        //heal
+        case 2:
+            monsterheal(hp_monster, alea);
+            break;
+        //defence
+        case 3:
+            monsterdefence();
+            break;
+    }
+    turndef_p -= 1;
+    if (turndef_p <= 0) defence_p = 0;
+    turn = 1;
+    iturn += 1;
+}
+function turngestion(activeCard) {
+    if (turn === 1) playerturn(activeCard);
+    else if (turn === 2) monsterturn(Math.floor(Math.random() * 3 + 1));
+    else alert("error");
+}
+exports.default = turngestion;
 
 },{"./allgo":"hRUZT","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"hRUZT":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
@@ -795,7 +857,7 @@ const ATTACK_RANGE = [
     1,
     1
 ];
-function calculateDamage(attack, defense, isBuff, isDeBuff) {
+function calculateDamage(attack, defence, isBuff, isDeBuff) {
     let damage = (Math.random() * (MIN_DAMAGE - MAX_DAMAGE) + MIN_DAMAGE) * attack;
     let damageModifier = (isBuff ? BUFF_MULTIPLIER : 1) * (isDeBuff ? DEBUFF_MULTIPLIER : 1);
     damage = Math.floor(damage * damageModifier);
@@ -808,24 +870,24 @@ function calculateDamage(attack, defense, isBuff, isDeBuff) {
     if (isMiss) {
         damage = 0;
         alert("miss!");
-    } else damage = Math.max(Math.floor(damage - defense), 1);
+    } else damage = Math.max(Math.floor(damage - defence * 0.7), 1);
     return damage;
 }
-function finaledegat(attack, defense, isBuff, isDeBuff) {
+function finaledegat(attack, defence, isBuff, isDeBuff) {
     let finalDamage = 0;
     let attackCount = ATTACK_RANGE[1] - ATTACK_RANGE[0] + 1;
-    for(let i = 0; i < attackCount; i++)finalDamage += calculateDamage(attack, defense, isBuff, isDeBuff);
+    for(let i = 0; i < attackCount; i++)finalDamage += calculateDamage(attack, defence, isBuff, isDeBuff);
     finalDamage = Math.floor(finalDamage / attackCount);
     return finalDamage;
 }
-function allgo(attack, defense, heal, isBuff, isDeBuff) {
-    if (attack === 0 && defense === 0 && heal > 0) //let nbrheal = healcalculate(heal)
+function allgo(attack, defence, heal, isBuff, isDeBuff) {
+    if (attack === 0 && heal > 0) //let nbrheal = healcalculate(heal)
     //return (nbrheal + 100);
     return 0;
     else if (attack > 0 && heal === 0) {
-        let nbrdegat = finaledegat(attack, defense, isBuff, isDeBuff);
+        let nbrdegat = finaledegat(attack, defence, isBuff, isDeBuff);
         return nbrdegat;
-    } else alert("cards error");
+    } else console.log("cards de defense ou cards inutile");
     return 0;
 }
 exports.default = allgo;
