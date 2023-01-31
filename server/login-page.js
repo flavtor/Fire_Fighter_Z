0f946fb359570afff772cbb7b896c5a3804ea691
agahakan <qirahakan@gmail.com>
2023-01-31 17:54:57 +0100

    add: registration form and register function

--- server/login-page.js
+++ server/login-page.js
@@ -2,6 +2,10 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
+const registerForm = document.getElementById("register-form");
+const registerButton = document.getElementById("register-form-submit");
+const registerErrorMsg = document.getElementById("register-error-msg");
+
 // Service REST /login parameter username, password 
 async function login(){
 	const username = loginForm.username.value;
@@ -27,4 +31,25 @@ async function login(){
         // Otherwise, make the login error message show (change its oppacity)
         loginErrorMsg.style.opacity = 1;
     }
+}
+
+async function register(){
+	const username = registerForm.username.value;
+	const password = registerForm.password.value;
+
+	let response = await fetch('http://localhost:8080/adduser?username='+username+'&password='+password, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	let data = await response.json()
+	
+	if (data["status"] == "ok"){
+		localStorage.setItem("username", data["username"]);
+		window.location.href = "testApi.html";
+	}
+	else {
+		registerErrorMsg.style.opacity = 1;
+	}
 }
